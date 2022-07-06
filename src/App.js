dcd72bea7014aff4f5f5310790f08883f2008bd4
koczooR <koczorowski94@gmail.com>
2022-07-06 23:21:31 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
+
+export const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
