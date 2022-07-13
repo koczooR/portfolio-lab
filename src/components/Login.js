ae08f4f4c41391b7aa0bcaeea94822e0a2fc36da
koczooR <koczorowski94@gmail.com>
2022-07-13 16:04:16 +0200

    login and register validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,46 @@
 import { HeaderNav } from "./HeaderNav";
 import decoration from "../assets/Decoration.svg";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const Login = () => {
+  const defaultState = { email: "", password: "" };
+  const [formValues, setFormValues] = useState(defaultState);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!values.email) {
+      errors.email = "Podany email jest nieprawidłowy!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Podany email ma nieprawidłowy format!";
+    }
+
+    if (values.password.length < 6) {
+      errors.password = "Podane hasło jest za krótkie!";
+    }
+    return errors;
+  };
+
   return (
     <>
       <header>
@@ -11,24 +49,25 @@ export const Login = () => {
       <section className="loginContainer">
         <span>Zaloguj się</span>
         <img src={decoration} alt="Decoration"></img>
-        <div className="loginBox">
-          <form>
+
+        <form onSubmit={handleSubmit}>
+          <div className="loginBox">
             <label>
               Email
-              <input type="text" name="email"></input>
+              <input type="text" name="email" value={formValues.email} onChange={handleChange}></input>
             </label>
+            <p className="error">{formErrors.email}</p>
             <label>
               Hasło
-              <input type="password" name="password"></input>
+              <input type="password" name="password" value={formValues.password} onChange={handleChange}></input>
             </label>
-          </form>
-        </div>
-        <div className="loginButtons">
-          <Link to="/rejestracja">Załóż konto</Link>
-          <a href="/" className="btn">
-            Zaloguj się
-          </a>
-        </div>
+            <p className="error">{formErrors.password}</p>
+          </div>
+          <div className="loginButtons">
+            <Link to="/rejestracja">Załóż konto</Link>
+            <button className="btn">Zaloguj się</button>
+          </div>
+        </form>
       </section>
     </>
   );
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,51 @@
 import { HeaderNav } from "./HeaderNav";
 import decoration from "../assets/Decoration.svg";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const Register = () => {
+  const defaultState = { email: "", password: "", password2: "" };
+  const [formValues, setFormValues] = useState(defaultState);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!values.email) {
+      errors.email = "Podany email jest nieprawidłowy!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "Podany email ma nieprawidłowy format!";
+    }
+
+    if (values.password.length < 6) {
+      errors.password = "Podane hasło jest za krótkie!";
+    }
+
+    if (values.password !== values.password2) {
+      errors.password2 = "Hasła muszą być takie same!";
+    } else if (values.password2.length < 6) {
+      errors.password2 = "Podane hasło jest za krótkie!";
+    }
+    return errors;
+  };
   return (
     <>
       <header>
@@ -11,28 +54,30 @@ export const Register = () => {
       <section className="loginContainer">
         <span>Załóż konto</span>
         <img src={decoration} alt="Decoration"></img>
-        <div className="loginBox">
-          <form>
+
+        <form onSubmit={handleSubmit}>
+          <div className="loginBox">
             <label>
               Email
-              <input type="text" name="email"></input>
+              <input type="text" name="email" value={formValues.email} onChange={handleChange}></input>
             </label>
+            <p className="error">{formErrors.email}</p>
             <label>
               Hasło
-              <input type="password" name="password"></input>
+              <input type="password" name="password" value={formValues.password} onChange={handleChange}></input>
             </label>
+            <p className="error">{formErrors.password}</p>
             <label>
               Powtórz hasło
-              <input type="password" name="password"></input>
+              <input type="password" name="password2" value={formValues.password2} onChange={handleChange}></input>
             </label>
-          </form>
-        </div>
-        <div className="loginButtons">
-          <Link to="/logowanie">Zaloguj się</Link>
-          <a href="/" className="btn">
-            Załóż konto
-          </a>
-        </div>
+            <p className="error">{formErrors.password2}</p>
+          </div>
+          <div className="loginButtons">
+            <Link to="/logowanie">Zaloguj się</Link>
+            <button className="btn">Załóż konto</button>
+          </div>
+        </form>
       </section>
     </>
   );
